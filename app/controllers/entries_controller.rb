8ac0f9c607232fe7bf30aac947fd78753fb1d07c
Ari <avogel@mit.edu>
2013-12-24 15:45:08 -0800

    fix authenticity token check for process_text

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,8 @@
 class EntriesController < ApplicationController
     before_action :set_entry, only: [:show, :edit, :update, :destroy]
     before_action :require_login, only: [:index, :show, :edit, :destroy]
+
+    skip_before_filter :verify_authenticity_token, :only => [:process_text]
     # GET /entries
     # GET /entries.json
     def index
